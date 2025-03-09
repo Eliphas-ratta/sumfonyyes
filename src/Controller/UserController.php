c38870ab9f6faa9fdeb01d784f6cb9e9ea53b1b0
Eliphas-ratta <kendall.oliver.pro@gmail.com>
2025-03-09 20:24:47 +0100

    Initial commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'user_index')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
